1a6caf8628d2be9268cedfd1efe568bdb1acf013
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-21 18:19:56 +0100

    Wait for API usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { fetchNews } from "@/lib/fetchNews";
 import { categories } from "../../constants";
 import NewsList from "@/components/Article/NewsList";
+import response from "../../response.json";
 export default async function Home() {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  // const news: NewsResponse =
+  //   response || (await fetchNews(categories.join(",")));
+
+  const news = response;
 
   console.log(news);
 
