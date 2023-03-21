49e73d35664de4d33424e013877e6297688872e9
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-21 10:43:01 +0100

    Added StepZen and queries

--- src/lib/fetchNews.ts
+++ src/lib/fetchNews.ts
@@ -8,3 +8,5 @@ export const fetchNews = async (
   //sort function by images vs not images present
   //return res
 };
+
+//stepzen import curl "https://aguablancaiturbide.stepzen.net/version" --header "Authentication: aguablancaiturbide::stepzen.io+1000::71ac92f8421a1d74fefcc3be45541d6d7711333300a426c89ce3c1b121339982"
